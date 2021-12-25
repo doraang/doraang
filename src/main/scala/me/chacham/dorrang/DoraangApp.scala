75f26ee414eaffad0dc9dd31c12f9e752a66175e
chacham <ccm0702@naver.com>
2021-12-25 15:40:02 +0900

    Use actorContext rather than creating new ActorSystem

--- src/main/scala/me/chacham/dorrang/DoraangApp.scala
+++ src/main/scala/me/chacham/dorrang/DoraangApp.scala
@@ -1,20 +1,32 @@
 package me.chacham.dorrang
 
 import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives.{complete, concat, path, pathPrefix, post}
 import akka.http.scaladsl.server.Route
-import me.chacham.dorrang.api.RootRoute
+import me.chacham.dorrang.api.ws.WebSocketRoute
 
+import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
 
 object DoraangApp {
-  private def startHttpServer(
-      routes: Route
-  )(implicit system: ActorSystem[_]): Unit = {
-    import system.executionContext
+  def route(context: ActorContext[_]): Route = {
+    concat(
+      path("room") {
+        post {
+          complete("DONE")
+        }
+      },
+      pathPrefix("ws") { WebSocketRoute(context) }
+    )
+  }
 
-    val futureBinding = Http().newServerAt("localhost", 8080).bind(routes)
+  private def startHttpServer(implicit context: ActorContext[_]): Unit = {
+    implicit val system: ActorSystem[Nothing] = context.system
+    implicit val executionContext: ExecutionContextExecutor = context.executionContext
+
+    val futureBinding = Http()(system).newServerAt("localhost", 8080).bind(route(context))
     futureBinding.onComplete {
       case Success(binding) =>
         val address = binding.localAddress
@@ -31,9 +43,9 @@ object DoraangApp {
 
   def main(args: Array[String]): Unit = {
     val rootBehavior = Behaviors.setup[Nothing] { context =>
-      startHttpServer(RootRoute())(context.system)
+      startHttpServer(context)
       Behaviors.empty
     }
-    val system = ActorSystem[Nothing](rootBehavior, "HelloAkkaHttpServer")
+    val system = ActorSystem[Nothing](rootBehavior, "DoraangHttpServer")
   }
 }
--- src/main/scala/me/chacham/dorrang/api/ws/WebSocketRoute.scala
+++ src/main/scala/me/chacham/dorrang/api/ws/WebSocketRoute.scala
@@ -1,10 +1,10 @@
 package me.chacham.dorrang.api.ws
 
 import akka.NotUsed
-import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
-import akka.http.scaladsl.server.Directives.{handleWebSocketMessages, path, pathPrefix}
+import akka.http.scaladsl.server.Directives.{handleWebSocketMessages, path}
 import akka.http.scaladsl.server.PathMatchers.Segment
 import akka.http.scaladsl.server.Route
 import akka.stream.OverflowStrategy
@@ -15,55 +15,54 @@ import me.chacham.dorrang.session._
 import java.util.UUID
 
 object WebSocketRoute {
-  private implicit val actorSystem: akka.actor.ActorSystem =
-    akka.actor.ActorSystem()
-
-  val route: Route = pathPrefix("ws") {
-    path(Segment) { roomId: String =>
-      handleWebSocketMessages(newSession(roomId))
+  def apply(context: ActorContext[_]): Route =
+    path(Segment) { (roomId: String) =>
+      val sessionHandler = WebSocketRoute.newSessionHandler(roomId)(context)
+      handleWebSocketMessages(sessionHandler)
     }
+
+  def newSessionHandler(roomId: String)(context: ActorContext[_]): Flow[Message, Message, NotUsed] = {
+    implicit val actorSystem: ActorSystem[Nothing] = context.system
+
+    val sessionId = UUID.randomUUID().toString
+
+    val (sourceActor: ActorRef[ClientMessage], rawSessionSource: Source[ClientMessage, NotUsed]) =
+      ActorSource
+        .actorRef(
+          PartialFunction.empty,
+          PartialFunction.empty,
+          1000,
+          OverflowStrategy.dropNew
+        )
+        .preMaterialize()
+    val source: Source[Message, NotUsed] = rawSessionSource.via(Flow.fromFunction[ClientMessage, Message] {
+      case NormalMessage(body) => TextMessage(body)
+    })
+
+    val session = context.spawn(Session(sessionId, sourceActor), sessionId)
+
+    val sinkActor: ActorRef[SessionMessage] = context.spawn(
+      Behaviors.receive[SessionMessage]((_, message) => {
+        println(s"Received $message")
+        session ! message
+        Behaviors.same
+      }),
+      sessionId
+    )
+
+    val sink: Sink[Message, NotUsed] =
+      Flow[Message]
+        .filter(_.isInstanceOf[TextMessage])
+        .flatMapConcat(message => message.asInstanceOf[TextMessage].textStream)
+        .map(message => FromClient(message))
+        .to(
+          ActorSink.actorRef[SessionMessage](
+            sinkActor,
+            Finished,
+            e => Failed(e)
+          )
+        )
+
+    Flow.fromSinkAndSource(sink, source)
   }
-
-  def newSession: String => Flow[Message, Message, Any] =
-    (roomId: String) => {
-      val (sourceActor: ActorRef[ClientMessage], rawSessionSource: Source[ClientMessage, NotUsed]) =
-        ActorSource
-          .actorRef(
-            PartialFunction.empty,
-            PartialFunction.empty,
-            1000,
-            OverflowStrategy.dropNew
-          )
-          .preMaterialize()
-      val source: Source[Message, NotUsed] = rawSessionSource.via(Flow.fromFunction[ClientMessage, Message] {
-        case NormalMessage(body) => TextMessage(body)
-      })
-
-      val sessionId = UUID.randomUUID().toString
-      val session = ActorSystem(Session(sessionId, sourceActor), sessionId)
-
-      val sinkActor: ActorRef[SessionMessage] = ActorSystem(
-        Behaviors.receive[SessionMessage]((_, message) => {
-          println(s"Received $message")
-          session ! message
-          Behaviors.same
-        }),
-        sessionId
-      )
-
-      val sink: Sink[Message, NotUsed] =
-        Flow[Message]
-          .filter(_.isInstanceOf[TextMessage])
-          .flatMapConcat(message => message.asInstanceOf[TextMessage].textStream)
-          .map(message => FromClient(message))
-          .to(
-            ActorSink.actorRef[SessionMessage](
-              sinkActor,
-              Finished,
-              e => Failed(e)
-            )
-          )
-
-      Flow.fromSinkAndSource(sink, source)
-    }
 }
